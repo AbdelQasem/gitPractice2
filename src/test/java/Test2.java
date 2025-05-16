7c9267ca2712a4358d74f7736300d0aa889ec2e8
AbdelQasem <abdeqasem@gmail.com>
2025-05-15 21:38:40 -0400

    Second version

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,13 +1,13 @@
 public class Test2 {
     public static void main(String[] args) {
 
-        System.out.println("Version 1");
-        System.out.println("Version 1");
-        System.out.println("Version 1");
-        System.out.println("Version 1");
-        System.out.println("Version 1");
-        System.out.println("Version 1");
-        System.out.println("Version 1");
+        System.out.println("Version 2");
+        System.out.println("Version 2");
+        System.out.println("Version 2");
+        System.out.println("Version 2");
+        System.out.println("Version 2");
+        System.out.println("Version 2");
+        System.out.println("Version 2");
 
     }
 }
